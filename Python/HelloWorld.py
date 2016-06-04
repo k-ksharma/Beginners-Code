9ea1f07aaf249d553a708da70146b4f7938cf79e
Krishna Kumar Sharma <sharma16aug@users.noreply.github.com>
2016-06-04 14:36:43 +0530

    Create HelloWorld.py

---
+++ Python/HelloWorld.py
@@ -0,0 +1,14 @@
+##These are single line comments
+'''These are
+multiple line 
+comments'''
+
+
+print "Hello World" ##Printing Hello World
+
+
+##same in all versions. you can also use th given format
+##Uncomment the following code  and you will see two hello worlds printed on the screen
+
+##print ("Hello world") 
+##for ver 3.4
