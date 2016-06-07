fa7fc9282fdcb16d11ef40e0645dfc0abd3f7227
Krishna Kumar Sharma <sharma16aug@users.noreply.github.com>
2016-06-07 09:48:49 +0530

    Create hello.java

---
+++ Java/hello.java
@@ -0,0 +1,10 @@
+public class hello
+//define a class. In our code the class name will be same as the code name.but you can have any.
+{
+  public static void main(String args[])
+  //main body of the code, from where the execution starts
+  {
+    System.out.println("Hello World");
+    //system is class and out is the static member and println is the method of java package System.io
+  }
+}
